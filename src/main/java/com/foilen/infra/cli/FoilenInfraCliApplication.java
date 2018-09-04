3592b01c1a295a17a3f29339fa1fa920f2ff478a
Simon Levesque <simon@foilen.com>
2018-09-03 21:31:50 -0400

    Spring boot project

---
+++ src/main/java/com/foilen/infra/cli/FoilenInfraCliApplication.java
@@ -0,0 +1,12 @@
+package com.foilen.infra.cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoilenInfraCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoilenInfraCliApplication.class, args);
+	}
+}
---
+++ src/test/java/com/foilen/infra/cli/FoilenInfraCliApplicationTests.java
@@ -0,0 +1,16 @@
+package com.foilen.infra.cli;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoilenInfraCliApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
