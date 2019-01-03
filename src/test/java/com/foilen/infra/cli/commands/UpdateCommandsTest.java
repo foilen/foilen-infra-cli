fc5173bf9ead6fb9684e95bffcb6d6b0d810cbc9
Simon Levesque <simon@foilen.com>
2019-01-03 16:03:14 -0500

    Update copyright

--- src/test/java/com/foilen/infra/cli/commands/UpdateCommandsTest.java
+++ src/test/java/com/foilen/infra/cli/commands/UpdateCommandsTest.java
@@ -1,7 +1,7 @@
 /*
     Foilen Infra CLI
     https://github.com/foilen/foilen-infra-cli
-    Copyright (c) 2018-2018 Foilen (http://foilen.com)
+    Copyright (c) 2018-2019 Foilen (http://foilen.com)
 
     The MIT License
     http://opensource.org/licenses/MIT
