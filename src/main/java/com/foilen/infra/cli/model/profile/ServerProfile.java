c9e5b7805476a2fdc76d01b620bbbb66929f11d9
Simon Levesque <simon@foilen.com>
2022-10-10 20:02:48 +0000

    Update copyright

--- src/main/java/com/foilen/infra/cli/model/profile/ServerProfile.java
+++ src/main/java/com/foilen/infra/cli/model/profile/ServerProfile.java
@@ -1,7 +1,7 @@
 /*
     Foilen Infra CLI
     https://github.com/foilen/foilen-infra-cli
-    Copyright (c) 2018-2021 Foilen (https://foilen.com)
+    Copyright (c) 2018-2022 Foilen (https://foilen.com)
 
     The MIT License
     http://opensource.org/licenses/MIT
